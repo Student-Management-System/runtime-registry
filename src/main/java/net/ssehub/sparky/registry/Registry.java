e772a66c418f9f3f6b896bf1fb3cc629d6f51ffc
mspark <sparkm@uni-hildesheim.de>
2022-07-29 15:36:25 +0200

    init

---
+++ src/main/java/net/ssehub/sparky/registry/Registry.java
@@ -0,0 +1,14 @@
+package net.ssehub.sparky.registry;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Registry {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Registry.class, args);
+	}
+
+}
