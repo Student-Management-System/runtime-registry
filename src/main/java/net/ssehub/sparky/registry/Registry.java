6c4c98f1a0d11e000721b8dc10742965d7c9948a
mspark <sparkm@uni-hildesheim.de>
2022-08-11 11:19:14 +0200

    style: add checkstyle

--- src/main/java/net/ssehub/sparky/registry/Registry.java
+++ src/main/java/net/ssehub/sparky/registry/Registry.java
@@ -1,14 +1,25 @@
 package net.ssehub.sparky.registry;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Main class which configured spring and starts the eureka discovery service.
+ *
+ * @author spark
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class Registry {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Registry.class, args);
-	}
+    /**
+     * Starts spring boot application.
+     * 
+     * @param args java CMD arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Registry.class, args);
+    }
 
 }
--- src/test/java/net/ssehub/sparky/registry/RegistryTests.java
+++ src/test/java/net/ssehub/sparky/registry/RegistryTests.java
@@ -5,10 +5,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+/**
+ * Simple registry test class.
+ * 
+ * @author spark
+ *
+ */
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class RegistryTests {
 
+    /**
+     * Test for loading springs application context (test for bean consistency).
+     */
     @Test
     public void contextLoads() {
     }
