92927cea8f84cc60c1e2fc41d946f3f838972466
mspark <sparkm@uni-hildesheim.de>
2022-10-17 16:22:07 +0200

    tests: handle context test as integration test

--- src/test/java/net/ssehub/e_learning/registry/RegistryIT.java
+++ src/test/java/net/ssehub/e_learning/registry/RegistryIT.java
@@ -1,9 +1,7 @@
 package net.ssehub.e_learning.registry;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * Simple registry test class.
@@ -11,9 +9,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
  * @author spark
  *
  */
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class RegistryTests {
+public class RegistryIT {
 
     /**
      * Test for loading springs application context (test for bean consistency).
